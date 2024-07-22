bc85b0e693cb179f35473d605609a9981d37b346
Filipe <lipeflorentino2@gmail.com>
2024-07-22 14:02:50 -0300

    ci: update github pipeline

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: ['main', 'next'],
+    branches: ['master'],
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
